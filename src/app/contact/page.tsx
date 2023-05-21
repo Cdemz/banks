0f2341c943a46f98b701d839cef7c303a8445c1e
cdemz <gbengaadepoju75@gmail.com>
2023-05-21 17:44:07 +0100

    fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -20,76 +20,90 @@ const ContactUs = () => {
         <div className=""></div>
       </main>
       {/* form  */}
-      <section className=" flex">
-        <div className="p-4 flex flex-col gap-3 ">
+      <section className="flex">
+        <div className="p-4 flex flex-col gap-3">
           {/* first name  */}
-          <div class="relative w-full mb-3 flex items-center gap-2">
-            <label class="block uppercase t text-xs font-bold mb-2" for="email">
+          <div className="relative w-full mb-3 flex items-center gap-2">
+            <label
+              className="block uppercase t text-xs font-bold mb-2"
+              htmlFor="email"
+            >
               First Name
             </label>
             <input
               type="email"
               className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder=" First Name"
+              placeholder="First Name"
             />
           </div>
           {/* last name  */}
 
-          <div class="relative w-full mb-3 flex items-center ">
-            <label class=" uppercase text-xs font-bold mb-2" for="email">
+          <div className="relative w-full mb-3 flex items-center">
+            <label className="uppercase text-xs font-bold mb-2" htmlFor="email">
               Last Name
             </label>
             <input
               type="email"
-              class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder=" Last Name"
+              className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+              placeholder="Last Name"
             />
           </div>
 
           {/* email  */}
-          <div class="relative w-full mb-3 flex items-center gap-2">
-            <label class="block uppercase t text-xs font-bold mb-2" for="email">
+          <div className="relative w-full mb-3 flex items-center gap-2">
+            <label
+              className="block uppercase t text-xs font-bold mb-2"
+              htmlFor="email"
+            >
               Email
             </label>
             <input
               type="email"
-              class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+              className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
               placeholder="Email"
             />
           </div>
           {/* last name  */}
 
           {/* phone */}
-          <div class="relative w-full mb-3 flex items-center gap-2">
-            <label class=" uppercase t text-xs font-bold mb-2" for="email">
+          <div className="relative w-full mb-3 flex items-center gap-2">
+            <label
+              className="uppercase t text-xs font-bold mb-2"
+              htmlFor="email"
+            >
               Phone
             </label>
             <input
               type="email"
-              class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+              className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
               placeholder="Phone"
             />
           </div>
           {/* event date */}
-          <div class="relative w-full mb-3 flex items-center gap-2">
-            <label class=" uppercase t text-xs font-bold mb-2" for="email">
+          <div className="relative w-full mb-3 flex items-center gap-2">
+            <label
+              className="uppercase t text-xs font-bold mb-2"
+              htmlFor="email"
+            >
               Project type
             </label>
             <input
               type="email"
-              class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+              className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
               placeholder="What is your next project?"
             />
           </div>
           {/* about your event  */}
-          <div class="relative w-full mb-3 flex items-center gap-2">
+          <div className="relative w-full mb-3 flex items-center gap-2">
             <label
-              class=" uppercase t text-xs font-bold mb-2"
-              for="email"
-            ></label>
+              className="uppercase t text-xs font-bold mb-2"
+              htmlFor="email"
+            >
+              About your project
+            </label>
             <input
               type="email"
-              class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full h-[8em] placeholder-top"
+              className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full h-[8em] placeholder-top"
               placeholder="Feed us with a little more detail"
             />
           </div>
@@ -102,12 +116,12 @@ const ContactUs = () => {
             banksconcrete@gmail.com.
           </p>
         </div>
-        <div className="hidden  md:flex md:w-[40%]">
+        <div className="hidden md:flex md:w-[40%]">
           <Image src="/bg-img.png" alt="" width={300} height={300} />
         </div>
       </section>
       {/* copy copy  */}
-      <div className="p-4 flex justify-around ">
+      <div className="p-4 flex justify-around">
         <Image src="/hero.png" alt="" width={300} height={300} />
       </div>
       <div className="py-[4em] bg-black text-white px-4 text-xs font-bold text-center md:text-2xl">
